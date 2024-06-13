6c5b023f850319c843e4deb14baaf7d78e8f41f2
Mark Rowel Tanio <work.marktanio@gmail.com>
2024-06-14 07:35:54 +0800

    updated files

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+//get elements and store them
+const quoteBox = document.querySelector('.quote');
+const authorBox = document.querySelector('.author');
+const button = document.querySelector('.quote-btn');
+
+const quotesArray = [
+    { author: "Tiktok", quote: "If you're tired, just give up" },
+    { author: "Tiktok1", quote: "Together we stand, Divided by four" },
+    { author: "Tiktok3", quote: "Do your best and God will give you rest." },
+    { author: "Tiktok4", quote: "Absence make the heart grow fondue" },
+    { author: "Tiktok5", quote: "Roses are red, violence is never the answer." },
+    { author: "Tiktok6", quote: "Ngayong malaayaaa ka naaaa . magagawa mo naaaaa" }
+];
+
+button.addEventListener('click',function(){
+    const randomNumber = Math.floor(Math.random()* quotesArray.length);
+    const randomQuote = quotesArray[randomNumber];
+    quoteBox.textContent = `"${randomQuote.quote}"`
+    authorBox.textContent = `-${randomQuote.author}`;
+});
+
+});
